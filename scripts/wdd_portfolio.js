3687e780229d9519b1f4d9a4c648769a169dc3cc
Mark Tobler <marktobler@gmail.com>
2020-04-28 19:40:18 -0700

    Menu changes

--- scripts/wdd_portfolio.js
+++ scripts/wdd_portfolio.js
@@ -3,6 +3,7 @@ File: wdd_portfolio
 Author: Mark Tobler
 Description: Javascript for WDD330 portfolio and notebook
 */
+// These need to be defined before they can be referenced.
 const closeMenu = () => {
    document.getElementById("menu").style.width = "0%";
 };
@@ -14,11 +15,3 @@ const openMenu = () => {
 document.getElementById('close').addEventListener('click', closeMenu);
 document.getElementById('menu_open').addEventListener('click', openMenu);
 
-function openNav() {
-   document.getElementById("myNav").style.width = "100%";
-}
-
- /* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-   document.getElementById("myNav").style.width = "0%";
-}
