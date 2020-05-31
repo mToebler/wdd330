7f382ac7fa5cf4818f387e77fd6e9bb80bb0dae1
Mark Tobler <marktobler@gmail.com>
2020-05-31 16:34:44 -0700

    CSS fixes, moved filtered logic to top.

--- project1/js/todoview.js
+++ project1/js/todoview.js
@@ -21,43 +21,47 @@ class TodoView {
 
    renderTasks(taskArray = [], filterObj = null) {
       // remember, the only point of contact between view and the outside 
-      // world is the controller sets up or sends.
+      // world should be what the controller sets up or sends.
       let renderedTasks = [];
       let taskUl = document.createElement('ul');
       taskUl.classList.add('todo-list');
       let isInstance = taskArray instanceof Array;
       console.log('is Array:', isInstance, taskArray);
 
-      // [].forEach.call(taskArray, obj =>
-      //    renderedTasks.push(this.renderTask(obj)));
+      // pushing filter logic first as the "jumping" controls are
+      // annoying.
+      renderedTasks.push(this.renderLogic(taskArray, filterObj));
 
       taskArray.forEach(
          obj =>
             renderedTasks.push(this.renderTask(obj)));
-
-      // add the filter <li>
-      let filterLogic =  `<li class="todo-item todo-logic ">
-                           <div>${renderedTasks.length} tasks shown</div>
-                           <div>display:</div>
-                           <div id="filter_all" data-action="show_all" class="${filterObj===null ? 'active-filter' : 'inactive-filter'}">all</div>
-                           <div id="filter_active" data-action="show_active" class="${filterObj===false ? 'active-filter' : 'inactive-filter'}">active</div>
-                           <div id="filter_done" data-action="show_done" class="${filterObj===true ? 'active-filter' : 'inactive-filter'}">completed</div>
-                        </li>`;
-      renderedTasks.push(filterLogic);
+   
       // concat them all together
       taskUl.innerHTML = renderedTasks.join("");
       // return renderedTasks;
       return taskUl;
    }
 
+   renderLogic(taskArray, filterObj) {
+      return (`
+               <li class="todo-item todo-logic ">
+                  <div>${taskArray.length} tasks shown</div>
+                  <div class="todo-display-div">Display:</div>
+                  <div id="filter_all" data-action="show_all" class="${filterObj===null ? 'active-filter' : 'inactive-filter'}">all</div>
+                  <div id="filter_active" data-action="show_active" class="${filterObj===false ? 'active-filter' : 'inactive-filter'}">active</div>
+                  <div id="filter_done" data-action="show_done" class="${filterObj===true ? 'active-filter' : 'inactive-filter'}">done</div>
+               </li>
+      `);
+   }
+
    renderTask(task) {
       return (`
-               <li class="todo-item ${task.getIsComplete() ? "checked" : ""}" data-id="${task.getId()}">               
-               <input type="checkbox" data-id="${task.getId()}" ${task.getIsComplete() ? "checked" : ""} />                
-               ${task.getContent()} 
-               <button class="remove-button" data-id="${task.getId()}" data-action="delete">&times;</button>
-               </li>
-               `);
+            <li class="todo-item ${task.getIsComplete() ? "checked" : ""}" data-id="${task.getId()}">               
+            <input type="checkbox" data-id="${task.getId()}" ${task.getIsComplete() ? "checked" : ""} />                
+            ${task.getContent()} 
+            <button class="remove-button" data-id="${task.getId()}" data-action="delete">&times;</button>
+            </li>
+      `);
    }
 
    toggleTask(target, isChecked) {
